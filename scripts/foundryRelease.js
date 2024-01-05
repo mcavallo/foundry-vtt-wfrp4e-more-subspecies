2205a54227fabe9b0c5804ff97209d72a528575d
Mariano Cavallo <mariano.cavallo@gmail.com>
2024-01-05 15:56:06 +0100

    Avoid using destructuring. Improve output

--- scripts/foundryRelease.js
+++ scripts/foundryRelease.js
@@ -3,8 +3,17 @@ import fetch from 'node-fetch';
 import path from 'path';
 
 async function sendRequest(payload, isDryRun = true) {
-  const body = { ...payload, 'dry-run': isDryRun };
-  console.log(`Sending request...`);
+  let body = Object.assign({}, payload);
+
+  if (isDryRun) {
+    body['dry-run'] = isDryRun;
+    console.log(`\nSending dry run request...`);
+  } else {
+    console.log(`\nSending request...`);
+  }
+
+  console.log(JSON.stringify(body, null, 2));
+
   const response = await fetch(
     'https://api.foundryvtt.com/_api/packages/release_version/',
     {
@@ -17,6 +26,16 @@ async function sendRequest(payload, isDryRun = true) {
     }
   );
   return await response.json();
+}
+
+function printErrors(response) {
+  const errorKeys = Object.keys(response.errors);
+
+  for (const errorKey of errorKeys) {
+    for (const error of response.errors[errorKey]) {
+      console.log(`Error (${errorKey}): ${error.message} (${error.code})`);
+    }
+  }
 }
 
 const run = async () => {
@@ -35,20 +54,22 @@ const run = async () => {
   const dryRunResponse = await sendRequest(payload, true);
 
   if (dryRunResponse.status === 'error') {
-    console.log(`Dry run failed.`);
-    const errorKeys = Object.keys(dryRunResponse.errors);
-
-    for (const errorKey of errorKeys) {
-      for (const error of dryRunResponse.errors[errorKey]) {
-        console.log(`Error (${errorKey}): ${error.message} (${error.code})`);
-      }
-    }
-
+    console.log(`\nDry run failed.`);
+    printErrors(dryRunResponse);
     return;
   }
 
-  console.log(`Dry run OK.`);
+  console.log(`\nDry run OK.`);
+
   const response = await sendRequest(payload, false);
+
+  if (response.status === 'error') {
+    console.log(`\nRelease failed.`);
+    printErrors(response);
+    return;
+  }
+
+  console.log(`\nRelease OK.`);
   console.log(JSON.stringify(response, null, 2));
 };
 
